22adf57fa66262c4ef48e8cec07e5beda3f0270f
vrishinvv <vrishinvv@gmail.com>
2021-11-06 13:21:57 +0530

    modified started pack

--- database/mongo.js
+++ database/mongo.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-//const { mongoURL } = require('@root/config.json');
-const mongoURL = process.env.mongoURL;
+const { mongoURL } = require('@root/config.json');
+//const mongoURL = process.env.mongoURL;
 
 module.exports = async () => {
-    await mongoose.connect(encodeURI(mongoURL), {
-        keepAlive: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    });
-    return mongoose;
+   await mongoose.connect(encodeURI(mongoURL), {
+      keepAlive: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+   });
+   return mongoose;
 };
